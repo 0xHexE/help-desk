ced86b33bea8c0601f7a91c40322d2e5aee672d8
Omkar Yadav <httpsomkar@gmail.com>
2019-10-17 21:26:04 +0400

    added endpoint for the is user avaialble

--- src/main/kotlin/com/f4erp/help_desk/restend/Onboard.kt
+++ src/main/kotlin/com/f4erp/help_desk/restend/Onboard.kt
@@ -1,16 +1,24 @@
 package com.f4erp.help_desk.restend
 
+import com.f4erp.help_desk.repositories.UserRepository
+import com.f4erp.help_desk.utils.validators.Authenticator
+import com.google.firebase.auth.FirebaseAuth
 import org.camunda.bpm.engine.RuntimeService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.client.HttpClientErrorException
 import java.util.*
 
 @RestController
-@RequestMapping("/onboard")
-class Onboard(@field:Autowired private val runtimeService: RuntimeService) {
+@RequestMapping("/f4erp/api/v1/onboard")
+class Onboard(
+        @field:Autowired private val runtimeService: RuntimeService,
+        @field:Autowired private val firebaseAuth: FirebaseAuth,
+        @field:Autowired private val userRepository: UserRepository
+) {
+    private val authenticator: Authenticator = Authenticator(this.firebaseAuth)
+
     @PostMapping
     fun createNewClient(
             @RequestBody body: Map<String, Any>
@@ -25,5 +33,18 @@ class Onboard(@field:Autowired private val runtimeService: RuntimeService) {
                         )
                 )
                 .processInstanceId
+    }
+
+    @GetMapping
+    fun isUserInDb(
+            @RequestHeader("Authorization") authorizationToken: String
+    ): Any {
+        val user = authenticator.checkIsAuthenticate(authorizationToken)
+                ?: throw HttpClientErrorException(HttpStatus.FORBIDDEN, "Unauthorized")
+        val userInfo = userRepository.findById(user.uid)
+        if (userInfo.isEmpty) {
+            return mapOf("error" to "User is not registered", "statusCode" to 401)
+        }
+        return userInfo.get()
     }
 }
